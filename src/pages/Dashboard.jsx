878bdfa1c2eb9be984d1c8c9f2ce458b9745d972
hasan <mmhotmail>
2023-11-27 10:01:44 +0300

    welcome

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -16,12 +16,19 @@ useEffect(() => {
 
 }, [])
 
-  return (
+  return (<>
+  {!data?.length && (
+        <div className="d-flex justify-content-center align-items-center profile-div">
+          <h3>
+           🎉🎉🎉 Welcome to BlogAPP🎉🎉🎉
+          </h3>
+        </div>
+      )}
+    
     <div className="row justify-content-center w-100 my-3 mx-0 gap-4">
       {data?.map((item)=>(<div key={item.id} className="col-9 col-md-4 col-lg-3 justify-content-center d-flex"><Card   {...item} getCard={getCard} /></div>))}
     </div>
-
-    
+    </>
   )
 }
 
